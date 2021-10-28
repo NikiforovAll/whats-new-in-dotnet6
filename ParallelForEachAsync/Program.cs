8cca92c5c8b7fd8c9b8508de1df2d9658b5ef823
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2021-10-29 01:04:51 +0300

    Add ParallelForEachAsync

---
+++ ParallelForEachAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+await Parallel.ForEachAsync(Generate(), Handle)
+    .WaitAsync(TimeSpan.FromMilliseconds(200))
+    .ContinueWith(Report);
+
+static async IAsyncEnumerable<int> Generate()
+{
+    while (true)
+    {
+        var delay = Random.Shared.Next(100);
+        WriteLine($"Issued {delay}");
+        await Task.Delay(delay);
+        yield return delay;
+    }
+}
+
+static async ValueTask Handle(int i, CancellationToken ct)
+{
+    await Task.Delay(i, ct);
+    WriteLine($"Handled {i}");
+}
+
+static void Report(Task t) => WriteLine(
+    $"Finished at: {DateTime.Now:T}, task.Status {t.Status}");

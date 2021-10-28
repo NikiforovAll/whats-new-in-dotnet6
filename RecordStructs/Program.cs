1ee1fb96df8a44aa67a7ea1bec5c3b8339a157a7
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2021-10-29 00:08:44 +0300

    Add RecordStructs

---
+++ RecordStructs/Program.cs
@@ -0,0 +1,27 @@
+﻿
+public record class Point(double X, double Y, double Z);
+
+// public record class Point
+// {
+//     public double X {  get; init; }
+//     public double Y {  get; init; }
+//     public double Z {  get; init; }
+// }
+
+public readonly record struct Point2(double X, double Y, double Z);
+
+// public record struct Point2
+// {
+//     public double X {  get; init; }
+//     public double Y {  get; init; }
+//     public double Z {  get; init; }
+// }
+
+public record struct Point3(double X, double Y, double Z);
+
+// public record struct Point3
+// {
+//     public double X { get; set; }
+//     public double Y { get; set; }
+//     public double Z { get; set; }
+// }

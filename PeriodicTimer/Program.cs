7959a997c66cee669b1470f220a9ee783c3c0b45
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2021-10-29 10:49:02 +0300

    Add PeriodicTimer

---
+++ PeriodicTimer/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using static System.Numerics.BitOperations;
+
+using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+while (await timer.WaitForNextTickAsync())
+{
+    LogEntry? l = Random.Shared.Next(128) is int r && IsPow2(r)
+        ? default
+        : new(r);
+
+    ProcessLogEntry(l);
+}
+
+
+static void ProcessLogEntry(LogEntry? l)
+{
+    ArgumentNullException.ThrowIfNull(l, nameof(l));
+    WriteLine(l);
+}
+
+public record class LogEntry(int Value);
